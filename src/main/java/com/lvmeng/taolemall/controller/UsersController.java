f6b1da1421eb4051cb7198fefeae126d200bed5d
feidao <feidao@feidaodeMacBook-Pro.local>
2021-02-18 13:19:57 +0800

    注册接口完成

--- src/main/java/com/lvmeng/taolemall/controller/UsersController.java
+++ src/main/java/com/lvmeng/taolemall/controller/UsersController.java
@@ -1,5 +1,6 @@
 package com.lvmeng.taolemall.controller;
 
+import com.lvmeng.taolemall.Users;
 import com.lvmeng.taolemall.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,33 @@ public class UsersController {
     public ModelAndView login(String username , String password){
         ModelAndView mav = new ModelAndView(new MappingJackson2JsonView());
         mav.addObject("user",userService.login(username, password));
+        return mav;
+
+    }
+
+
+    @RequestMapping(value = "/register")
+    public ModelAndView register(String username, String password, String user_address){
+        Users userByName = userService.findUserByName(username);
+        ModelAndView mav = new ModelAndView(new MappingJackson2JsonView());
+        if(userByName != null){
+            mav.addObject("flag", false);
+            mav.addObject("user", "用户名重复");
+            return mav;
+        }
+        //count返回1为插入数据成功，0为失败
+        int count;
+        try{
+            count = userService.register(username, password, user_address);
+        }catch (Exception e){
+            count = 0;
+        }
+        if(count == 1){
+            mav.addObject("flag", true);
+            mav.addObject("user", userService.login(username, password));
+        }else{
+            mav.addObject("flag", false);
+        }
         return mav;
 
     }
